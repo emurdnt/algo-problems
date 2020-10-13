17d4e189a2c5b08a811c9360c27db8ffa5e3a6db
Em Urdaneta <em.urdaneta94@gmail.com>
2020-10-13 19:11:50 +0000

    reverse num in leetcode

---
+++ reverse.js
@@ -0,0 +1,8 @@
+const reverse = (x) => {
+  const limit = 2147483648;
+  const k = x < 0 ? -1 : 1;
+  const n = Number(String(Math.abs(x)).split("").reverse().join(""));
+  return n > limit ? 0 : n * k;
+};
+
+console.log(reverse(1534236469));
